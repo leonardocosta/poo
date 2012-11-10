8d25b6ecc59ce56f3a485b6435862e4e3a758faf
Leonardo <leonardo.costa@dce.ufpb.br>
2012-11-10 18:19:23 -0300

    adicionando exercicio 3

---
+++ Exercicio3/ProgramaPrincipal.java
@@ -0,0 +1,51 @@
+
+
+import java.util.Scanner;
+
+public class ProgramaPrincipal {
+	public static void main(String [] args){
+		Scanner teclado = new Scanner(System.in);
+		
+		Pedido pedido = new Pedido(01);
+		ItemDePedido item;
+		
+		int continua=1;
+		double valorTotal=0;
+		
+		
+		do{
+			System.out.println("Digite o código do Produto: ");
+			String codigo = teclado.next();
+						
+			System.out.println("Quantidade de item: ");
+			int quantidade = teclado.nextInt();
+					
+			System.out.println("Valor o unitário do ítem: ");
+			double valor = teclado.nextDouble();
+			
+			item = new ItemDePedido (codigo, quantidade, valor );
+			
+			if(codigo.equals("0")){
+				continua=0;
+			}
+			
+			valorTotal+=valor;
+			
+		
+	}while(continua==1);
+						
+		
+		System.out.println(valorTotal);
+		
+						
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+	}
+
